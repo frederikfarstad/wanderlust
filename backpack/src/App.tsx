7b5d8dc1831ecf024633ad83212c3f5100702b1d
Alexander Rathke Johansen <alexarj@stud.ntnu.no>
2023-02-08 09:48:59 +0100

    feat: :sparkles: Connect firebase to application and add firebase auth. Create LoginPage and add to App component.

--- backpack/src/App.tsx
+++ backpack/src/App.tsx
@@ -1,10 +1,27 @@
-function App() {
+import LoginPage from "./pages/LoginPage";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC_LQGKZRLoWdGzyYae1MmyuokXd8djqmk",
+  authDomain: "backpacking-b6486.firebaseapp.com",
+  projectId: "backpacking-b6486",
+  storageBucket: "backpacking-b6486.appspot.com",
+  messagingSenderId: "835942085080",
+  appId: "1:835942085080:web:ada768679df6ff61ddc2ad",
+  measurementId: "G-0WLQB3GX4H",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      Hello world!
-    </div>
-  )
+    <>
+      <LoginPage />
+    </>
+  );
 }
 
-export default App
+export default App;
