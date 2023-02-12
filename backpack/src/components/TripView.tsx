e3a6c762d5e608f249d09a74a62f7b40222200e5
Frederik Farstad <Frederf@stud.ntnu.no>
2023-02-12 19:06:57 +0100

    style: :lipstick: #3 Updated style

--- backpack/src/components/TripView.tsx
+++ backpack/src/components/TripView.tsx
@@ -9,7 +9,7 @@ function TripView() {
     /* Display own trips */
     return (
       <div className="font-mono font-bold min-h-[80vh] max-h-fit w-auto text-white border-l border-r mt-[10vh] mb-[10vh] pl-[2vh] pr-[2vh]">
-        <button onClick={() => setCheck(prevCheck => !prevCheck)}>
+        <button onClick={() => setCheck(prevCheck => !prevCheck)} className="border-2 p-2 rounded-lg bg-primary-400 mb-3 hover:animate-pulse">
           <p className="text-m">Bytt Til "Favoritter"</p>
         </button>
 
@@ -25,7 +25,7 @@ function TripView() {
     /* Display favorite trips */
     return (
       <div className="font-mono font-bold min-h-[80vh] max-h-fit  w-auto text-white border-l border-r mt-[10vh] mb-[10vh] pl-[2vh] pr-[2vh]">
-        <button onClick={() => setCheck(prevCheck => !prevCheck)}>
+        <button onClick={() => setCheck(prevCheck => !prevCheck)} className="border-2 p-2 rounded-lg bg-primary-400 mb-3 hover:animate-pulse">
           <p className="text-m">Bytt Til "Dine Turer"</p>
         </button>
           
--- backpack/src/pages/ProfilePage.tsx
+++ backpack/src/pages/ProfilePage.tsx
@@ -8,7 +8,7 @@ function ProfilePage() {
     return (
       <div className="grid smallScreen:grid-cols-1 grid-cols-3 h-screen bg-primary-300 overflow-scroll">
         <div id="gridOneProfile" className="h-screen">
-            <div className="rounded-full overflow-hidden w-1/2 aspect-square m-auto mb-[5vh] mt-[10vh] bg-gradient-to-br from-[#E54B4B] to-[#FFA987] p-2">
+            <div className="rounded-full overflow-hidden w-1/2 aspect-square m-auto mb-[5vh] mt-[10vh] bg-gradient-to-br from-primary-600 to-primary-400 p-2">
                 <div className="rounded-full overflow-hidden aspect-square">
                     <img src={siv}/>
                 </div>
@@ -16,7 +16,7 @@ function ProfilePage() {
             
             <div className="m-auto w-3/4 text-white">
                 <h1 className="text-3xl font-serif font-bold bg-inherit">Om meg</h1>
-                <div className="bg-gradient-to-br from-[#E54B4B] to-[#FFA987] rounded-3xl">
+                <div className="bg-gradient-to-br from-primary-600 to-primary-400 rounded-3xl">
                     <div className="border-4 border-primary-200 rounded-3xl min-h-[20vh] max-h-[40vh] overflow-auto pl-[1.25rem] pt-[0.5rem] pr-[1.25rem] text-xl">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
@@ -25,7 +25,7 @@ function ProfilePage() {
            
              
         </div>
-        <div id="gridTwoTrips" className="">
+        <div id="gridTwoTrips" className="col-span-2">
             <TripView />
         </div>
         <div id="gridThreeAdvertisement" className="">
