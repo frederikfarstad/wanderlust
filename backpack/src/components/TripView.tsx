b0250920fb9019f35ce9024cf09f20ed7a1814bb
Frederik Farstad <Frederf@stud.ntnu.no>
2023-02-13 13:57:10 +0100

    style: :lipstick: #3 Updated Style, support for backend
    Updated style, added animations, added a rating-system, made it easier to later implement backend

--- backpack/src/components/TripView.tsx
+++ backpack/src/components/TripView.tsx
@@ -3,41 +3,74 @@ import SmallTripView from './SmallTripView';
 
 function TripView() {
 
+  const ownDummyTrips = [
+    {
+    title: "Domen",
+    desc: "Domen gutt",
+    img:"",
+    rating:"4.26"
+    },
+    {
+    title: "Gløs",
+    desc: "Gløs gutt",
+    img:"",
+    rating:"5"
+    },
+    {
+    title: "Dragvoll",
+    desc: "Dragvoll gutt",
+    img:"",
+    rating:"1.27"
+    },
+    {
+    title: "Handels",
+    desc: "Handels gutt",
+    img:"",
+    rating:"3.75"
+    }
+  ]
+
+  const favDummyTrips = [
+    {
+    title: "Samf",
+    desc: "Samf gutt",
+    img: "",
+    rating:"4.99"
+    }
+  ]
+
   const [ownTrips, setCheck] = useState(true);
   
   if(ownTrips){
     /* Display own trips */
     return (
       <div className="font-mono font-bold min-h-[80vh] max-h-fit w-auto text-white border-l border-r mt-[10vh] mb-[10vh] pl-[2vh] pr-[2vh]">
-        <button onClick={() => setCheck(prevCheck => !prevCheck)} className="border-2 p-2 rounded-lg bg-primary-400 mb-3 hover:animate-pulse">
+        <button onClick={() => setCheck(prevCheck => !prevCheck)} className="border-2 p-2 rounded-lg bg-primary-400 mb-3 hover:bg-primary-300 transition-all ease-out duration-300">
           <p className="text-m">Bytt Til "Favoritter"</p>
         </button>
 
         <p className="text-3xl pb-[2vh]">Dine Turer:</p>
-
-        <SmallTripView />
-        <SmallTripView />
-        <SmallTripView />
-        <SmallTripView />
+          {ownDummyTrips.map((trip) => (
+            <SmallTripView {...[trip.title, trip.desc, trip.img, trip.rating]}/>
+          ))}
+        
       </div>
     )
   } else {
     /* Display favorite trips */
     return (
       <div className="font-mono font-bold min-h-[80vh] max-h-fit  w-auto text-white border-l border-r mt-[10vh] mb-[10vh] pl-[2vh] pr-[2vh]">
-        <button onClick={() => setCheck(prevCheck => !prevCheck)} className="border-2 p-2 rounded-lg bg-primary-400 mb-3 hover:animate-pulse">
+        <button onClick={() => setCheck(prevCheck => !prevCheck)} className="border-2 p-2 rounded-lg bg-primary-400 mb-3 hover:bg-primary-300 transition-all ease-out duration-300">
           <p className="text-m">Bytt Til "Dine Turer"</p>
         </button>
           
         <p className="text-3xl pb-[2vh] ">Dine Favoritter:</p>
-
-        <SmallTripView />
+        {favDummyTrips.map((trip) => (
+          <SmallTripView {...[trip.title, trip.desc, trip.img, trip.rating]} />
+        ))}
       </div>
     )
   }
-  
-  
-  
 }
   
 export default TripView
--- backpack/src/pages/ProfilePage.tsx
+++ backpack/src/pages/ProfilePage.tsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import siv from "../assets/siv.jpg"
-import SmallTripView from "../components/SmallTripView"
 import TripView from "../components/TripView"
+import "../index.css"
 
 function ProfilePage() {
 
     return (
       <div className="grid smallScreen:grid-cols-1 grid-cols-3 h-screen bg-primary-300 overflow-scroll">
-        <div id="gridOneProfile" className="h-screen">
+        <div id="gridOneProfile" className="h-[100vh] fade-in-hello">
             <div className="rounded-full overflow-hidden w-1/2 aspect-square m-auto mb-[5vh] mt-[10vh] bg-gradient-to-br from-primary-600 to-primary-400 p-2">
                 <div className="rounded-full overflow-hidden aspect-square">
                     <img src={siv}/>
                 </div>
             </div>
             
-            <div className="m-auto w-3/4 text-white">
+            <div className="m-auto w-3/4 text-white ">
                 <h1 className="text-3xl font-serif font-bold bg-inherit">Om meg</h1>
                 <div className="bg-gradient-to-br from-primary-600 to-primary-400 rounded-3xl">
-                    <div className="border-4 border-primary-200 rounded-3xl min-h-[20vh] max-h-[40vh] overflow-auto pl-[1.25rem] pt-[0.5rem] pr-[1.25rem] text-xl">
+                    <div className="border-4 border-primary-200 rounded-3xl min-h-[20vh] max-h-[40vh] overflow-auto pl-[1.25rem] pt-[0.5rem] pr-[1.25rem] text-xl scrollbar-hide">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
@@ -25,10 +25,10 @@ function ProfilePage() {
            
              
         </div>
-        <div id="gridTwoTrips" className="col-span-2">
+        <div id="gridTwoTrips" className="col-span-2 fade-in-hello">
             <TripView />
         </div>
-        <div id="gridThreeAdvertisement" className="">
+        <div id="gridThreeAdvertisement" className="fade-in-hello">
         
         </div>
       </div>
