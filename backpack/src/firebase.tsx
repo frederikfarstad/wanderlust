92ba430e153b455696c955224b2b81f152d6dfe9
Frederik Farstad <Frederf@stud.ntnu.no>
2023-02-21 12:53:17 +0100

    fix: :rewind: #3 removed previously implemented export on f_db

--- backpack/src/firebase.tsx
+++ backpack/src/firebase.tsx
@@ -28,7 +28,7 @@ const app = initializeApp(
   NODE_ENV === "test" ? testFirebaseConfig : firebaseConfig
 );
 
-export const f_db = getFirestore(app);
+const f_db = getFirestore(app);
 const f_auth = getAuth(app);
 
 if (NODE_ENV === "test") {
