d2e0d9d9e52c206e434ad3848bd52ac085b6c4d9
Hanne Fyrand <hannelf@stud.ntnu.no>
2023-03-06 10:49:58 +0100

    changed blue to primary

--- backpack/src/pages/ProfilePage.tsx
+++ backpack/src/pages/ProfilePage.tsx
@@ -24,34 +24,59 @@ function ProfilePage() {
   const { uid } = useUserInfo();
   let { id: urlID } = useParams();
 
-  const currentUsersProfile = uid === urlID
-  const { pfp, username} = getUserInfo(urlID as string)
+  const currentUsersProfile = uid === urlID;
+  const { pfp, username } = getUserInfo(urlID as string);
 
+  const [type, setType] = useState<"posts" | "likes" | "favorites">("posts");
 
-
-  const [type, setType] = useState<"posts" | "likes" | "favorites">("posts")
-
-  console.log(type)
+  console.log(type);
 
   const routes = getUserCollection(urlID as string, type);
 
-    const routeElemen = routes.length ? routes?.map((r) => <Post {...r} />) : <>No posts to display</>;
+  const routeElemen = routes.length ? (
+    routes?.map((r) => <Post {...r} />)
+  ) : (
+    <>No posts to display</>
+  );
 
+  console.log(routes);
 
-  
-  console.log(routes)
-
-  return <div className="bg-primary-300 p-20">
-    <div className="flex gap-4 items-center">
-      <img src={pfp} className="w-20 h-20 bg-primary-600 rounded-full" />
-      <div className="self-center text-xl font-semibold">{username}</div>
-      <button onClick={() => setType("posts")} type="button" className="ml-20 text-white h-12 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">{currentUsersProfile ? "My posts" : username + "'s posts" }</button>
-      <button onClick={() => setType("favorites")} type="button" className="ml-20 text-white h-12 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">{currentUsersProfile ? "My favourite posts" : username + "'s favourite posts" }</button>
-      {currentUsersProfile ? <button onClick={() => setType("likes")} type="button" className="ml-20 text-white h-12 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">My liked posts</button> : <></>}
+  return (
+    <div className="bg-primary-300 p-20">
+      <div className="flex gap-4 items-center">
+        <img src={pfp} className="w-20 h-20 bg-primary-600 rounded-full" />
+        <div className="self-center text-xl font-semibold">{username}</div>
+        <button
+          onClick={() => setType("posts")}
+          type="button"
+          className="ml-20 text-white h-12 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none"
+        >
+          {currentUsersProfile ? "My posts" : username + "'s posts"}
+        </button>
+        <button
+          onClick={() => setType("favorites")}
+          type="button"
+          className="ml-20 text-white h-12 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none"
+        >
+          {currentUsersProfile
+            ? "My favourite posts"
+            : username + "'s favourite posts"}
+        </button>
+        {currentUsersProfile ? (
+          <button
+            onClick={() => setType("likes")}
+            type="button"
+            className="ml-20 text-white h-12 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none"
+          >
+            My liked posts
+          </button>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className=" mt-20 grid grid-cols-3 gap-4">{routeElemen}</div>
     </div>
-    <div className=" mt-20 grid grid-cols-3 gap-4">{routeElemen}</div>
-  </div>;
+  );
 }
 
 export default ProfilePage;
-
