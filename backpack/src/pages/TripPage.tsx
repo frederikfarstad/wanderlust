443f2182a918ae1179067373a7c79b3a672586bc
sondrp <sondrp@stud.ntnu.no>
2023-03-15 20:51:03 +0100

    #24 feat: :sparkles: Create landing page for trip
    yet to display the actual trip, but shows ratings and add comment option

---
+++ backpack/src/pages/TripPage.tsx
@@ -0,0 +1,15 @@
+import CreateRating from "../components/rateTrip/CreateRating";
+import RatingSection from "../components/rateTrip/RatingSection";
+
+export default function TripPage() {
+  return (
+    <div className="grid grid-cols-6 min-h-screen bg-primary-300 pt-20 gap-8">
+      <div className="col-start-2 col-span-4">
+        <CreateRating />
+      </div>
+      <div className="col-start-2 col-span-4">
+        <RatingSection />
+      </div>
+    </div>
+  );
+}
