a0d29f609fadcbc9b6fc2fd25431b06c1bd75d57
Alexander Rathke Johansen <alexarj@stud.ntnu.no>
2023-03-16 13:16:58 +0100

    test: :white_check_mark: Finish writing tests for MainPage sorting functionality
    #16

--- backpack/src/test/MainPage.test.tsx
+++ backpack/src/test/MainPage.test.tsx
@@ -1,4 +1,4 @@
-import { act, prettyDOM, screen, waitFor } from "@testing-library/react";
+import { act, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import MainPage from "../pages/MainPage";
@@ -16,38 +16,155 @@ beforeEach(() => {
 
 describe("MainPage", () => {
   it("should allow adding trips to and removing trips from favorites", async () => {
-    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"), {
-      timeout: 5000,
-    });
+    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"));
     expect(tripDivs.length).toBeGreaterThan(0);
 
     const tripDiv = tripDivs[0];
 
-    const favoriteButton = await searchForChildElementWithQuery(
-      tripDiv,
-      "[title='FavoriteTripButton']"
-    );
+    const favoriteButton = await searchForChildElementWithQuery(tripDiv, "[title='FavoriteTripButton']");
     if (favoriteButton === null) return;
 
     await searchForChildElementWithQuery(tripDiv, "[title='NonFavoritedIcon']");
-    userEvent.click(favoriteButton);
+    await userEvent.click(favoriteButton);
     await searchForChildElementWithQuery(tripDiv, "[title='FavoritedIcon']");
-    userEvent.click(favoriteButton);
+    await userEvent.click(favoriteButton);
     await searchForChildElementWithQuery(tripDiv, "[title='NonFavoritedIcon']");
   });
 
-  it("should sort correctly from newest to oldest and oldest to newest", async () => {
+  it("should sort correctly from newest to oldest", async () => {
     const sortByNewButton = screen.getByTestId("SortingButton-New");
-    userEvent.click(sortByNewButton);
-    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"), {
-      timeout: 5000,
-    });
+    await userEvent.click(sortByNewButton);
+    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"));
 
     var prevLifetime = 0;
     tripDivs.forEach((tripDiv) => {
-      const postLifetimeInfoContainer = tripDiv.querySelector(
-        "[title='PostLifetimeInfoContainer']"
-      );
+      const postLifetimeInfoContainer = tripDiv.querySelector("[title='PostLifetimeInfoContainer']");
+      if (postLifetimeInfoContainer !== null) {
+        // Check that previous trip div and this trip div is correctly sorted
+        const thisLifetime = parseInt(postLifetimeInfoContainer.innerHTML);
+        if (isNaN(thisLifetime)) return;
+
+        expect(thisLifetime).toBeGreaterThanOrEqual(prevLifetime);
+
+        prevLifetime = thisLifetime;
+      }
+    });
+  });
+
+  it("should sort correctly from oldest to newest", async () => {
+    const sortByNewButton = screen.getByTestId("SortingButton-New");
+    await userEvent.click(sortByNewButton);
+    await userEvent.click(sortByNewButton);
+    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"));
+
+    var prevLifetime = Infinity;
+    tripDivs.forEach((tripDiv) => {
+      const postLifetimeInfoContainer = tripDiv.querySelector("[title='PostLifetimeInfoContainer']");
+      if (postLifetimeInfoContainer !== null) {
+        // Check that previous trip div and this trip div is correctly sorted
+        const thisLifetime = parseInt(postLifetimeInfoContainer.innerHTML);
+        if (isNaN(thisLifetime)) return;
+
+        expect(thisLifetime).toBeLessThanOrEqual(prevLifetime);
+
+        prevLifetime = thisLifetime;
+      }
+    });
+  });
+
+  it("should sort correctly from least to most expensive", async () => {
+    const sortButton = screen.getByTestId("SortingButton-Price");
+    await userEvent.click(sortButton);
+    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"));
+
+    var prevPrice = 0;
+    tripDivs.forEach((tripDiv, index) => {
+      const postPriceInfoContainer = tripDiv.querySelector("[title='PostPriceInfoContainer']");
+      if (postPriceInfoContainer !== null) {
+        // Check that previous trip div and this trip div is correctly sorted
+        const thisPrice = parseInt(postPriceInfoContainer.innerHTML);
+        if (isNaN(thisPrice)) return;
+
+        expect(thisPrice).toBeGreaterThanOrEqual(prevPrice);
+
+        prevPrice = thisPrice;
+      }
+    });
+  });
+
+  it("should sort correctly from most to least expensive", async () => {
+    const sortButton = screen.getByTestId("SortingButton-Price");
+    await userEvent.click(sortButton);
+    await userEvent.click(sortButton);
+    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"));
+
+    var prevPrice = Infinity;
+    tripDivs.forEach((tripDiv) => {
+      const postPriceInfoContainer = tripDiv.querySelector("[title='PostPriceInfoContainer']");
+      if (postPriceInfoContainer !== null) {
+        // Check that previous trip div and this trip div is correctly sorted
+        const thisPrice = parseInt(postPriceInfoContainer.innerHTML);
+        if (isNaN(thisPrice)) return;
+
+        expect(thisPrice).toBeLessThanOrEqual(prevPrice);
+
+        prevPrice = thisPrice;
+      }
+    });
+  });
+
+  it("should sort trips correctly from shortest to longest", async () => {
+    const sortButton = screen.getByTestId("SortingButton-Duration");
+    await userEvent.click(sortButton);
+    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"));
+
+    var prevDuration = 0;
+    tripDivs.forEach((tripDiv, index) => {
+      const postPriceInfoContainer = tripDiv.querySelector("[title='PostDurationInfoContainer']");
+      if (postPriceInfoContainer !== null) {
+        // Check that previous trip div and this trip div is correctly sorted
+        const thisDuration = parseInt(postPriceInfoContainer.innerHTML);
+        if (isNaN(thisDuration)) return;
+
+        expect(thisDuration).toBeGreaterThanOrEqual(prevDuration);
+
+        prevDuration = thisDuration;
+      }
+    });
+  });
+
+  it("should sort trips correctly from longest to shortest", async () => {
+    const sortButton = screen.getByTestId("SortingButton-Duration");
+    await userEvent.click(sortButton);
+    await userEvent.click(sortButton);
+    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"));
+
+    var prevDuration = Infinity;
+    tripDivs.forEach((tripDiv, index) => {
+      const postPriceInfoContainer = tripDiv.querySelector("[title='PostDurationInfoContainer']");
+      if (postPriceInfoContainer !== null) {
+        // Check that previous trip div and this trip div is correctly sorted
+        const thisDuration = parseInt(postPriceInfoContainer.innerHTML);
+        if (isNaN(thisDuration)) return;
+
+        expect(thisDuration).toBeLessThanOrEqual(prevDuration);
+
+        prevDuration = thisDuration;
+      }
+    });
+  });
+
+  it("should reset sorting direction when switching sorting modes", async () => {
+    const sortByNewButton = screen.getByTestId("SortingButton-New");
+    const sortByPriceButton = screen.getByTestId("SortingButton-Price");
+    await userEvent.click(sortByPriceButton);
+    await userEvent.click(sortByPriceButton);
+    await userEvent.click(sortByNewButton);
+    const tripDivs = await waitFor(() => screen.findAllByTitle("TripDiv"));
+
+    var prevLifetime = 0;
+    tripDivs.forEach((tripDiv) => {
+      const postLifetimeInfoContainer = tripDiv.querySelector("[title='PostLifetimeInfoContainer']");
       if (postLifetimeInfoContainer !== null) {
         // Check that previous trip div and this trip div is correctly sorted
         const thisLifetime = parseInt(postLifetimeInfoContainer.innerHTML);
